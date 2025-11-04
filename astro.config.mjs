594b07935bd88d6869050953737b85ef38b13601
pitysatr <162272085@qq.com>
2025-11-04 13:57:05 +0800

    pity3

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://pitysatr.pages.dev/",
+	site: "https://pitysatr.netlify.app/",
 
 	base: "/",
 	trailingSlash: "always",
